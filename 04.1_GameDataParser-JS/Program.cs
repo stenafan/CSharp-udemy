adbc610f46bf000af7d41ded8bae72be50724f02
stenafan <152386620+stenafan@users.noreply.github.com>
2024-09-24 19:53:07 +0000

    new files

---
+++ 04.1_GameDataParser-JS/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+Console.WriteLine("Enter the name of the file you want to read");
+
+
+   public class Game
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public double Rating { get; set; }
+    }
---
+++ 042_GameDataParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+Console.WriteLine("Enter the name of the file you want to read");
+var fileName = Console.Readline();
+
+
+   public class Game
+    {
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public double Rating { get; set; }
+    }
